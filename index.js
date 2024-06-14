4ddd467947a0ebc05419645c63f98ed77f7d333f
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-06-14 14:26:58 +0200

    added function start game and modified start game to render game. also added 3rd card

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector ("#cards-el")
+
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-function startGame() {
+function startGame() { //you want the html button to have onclick startgame() but after the start the function is reused so you change the name for calling on it(just to make it semantically correct)
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.innerHTML = `Cards: ${cards[0]} ${cards[1]}`
+    sumEl.innerHTML = `Sum: ${sum}`
+
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "Wohoo! You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
     console.log(message)
+    messageEl.innerText = message
+}
+
+function newCard() {
+    console.log("new card")
+
+    let thirdcard = 3
+    sum += thirdcard
+    // console.log(sum)
+    renderGame()
 }
